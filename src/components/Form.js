063944f98bdc010c4a82ff1cd8b770c5ee996138
chris <cseiter@gmail.com>
2021-05-23 21:12:29 -0400

    create object for text fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData,setFormData] = useState({
+    fistname:"John",
+    lastName:"Henry",
+  });
+  /* const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry"); */
 
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFormData({
+      ...formData,
+      firstName: event.target.value,
+    });
+/*     setFirstName(event.target.value); */
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    setFormData({
+      ...formData,
+      lastName: event.target.value,
+    });
+/*     setLastName(event.target.value); */
   }
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text"
+      onChange={handleFirstNameChange}
+      value={FormData.firstName}
+      />
+      <input type="text"
+      onChange={handleLastNameChange}
+      value={FormData.lastName}
+      />
+{/*       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <button type="submit">Submit</button> */}
     </form>
   );
 }
