e5f6e68f35ae1c7e0af5bfbaaf1c15b5083eed9c
chris <cseiter@gmail.com>
2021-05-23 21:24:12 -0400

    all text boxes us single function,removed comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,41 +2,32 @@ import React, {useState} from "react";
 
 function Form() {
   const [formData,setFormData] = useState({
-    fistname:"John",
+    firstName:"John",
     lastName:"Henry",
   });
-  /* const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry"); */
 
-  function handleFirstNameChange(event) {
-    setFormData({
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData ({
       ...formData,
-      firstName: event.target.value,
+      [name]:value,
     });
-/*     setFirstName(event.target.value); */
-  }
-
-  function handleLastNameChange(event) {
-    setFormData({
-      ...formData,
-      lastName: event.target.value,
-    });
-/*     setLastName(event.target.value); */
   }
 
   return (
     <form>
-      <input type="text"
-      onChange={handleFirstNameChange}
-      value={FormData.firstName}
+      <input
+      type="text"
+      name="firstName"
+      value={formData.firstName}
+      onChange={handleChange}
       />
       <input type="text"
-      onChange={handleLastNameChange}
-      value={FormData.lastName}
+      name="lastName"
+      value={formData.lastName}
+      onChange={handleChange}
       />
-{/*       <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button> */}
     </form>
   );
 }
