4bcea8c476e75a2383de675bff92f4eb4e9f8fa6
chris <cseiter@gmail.com>
2021-05-23 21:29:19 -0400

    added admin checkbox and submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,19 +4,28 @@ function Form() {
   const [formData,setFormData] = useState({
     firstName:"John",
     lastName:"Henry",
+    admin: false,
   });
 
   function handleChange(event) {
     const name = event.target.name;
-    const value = event.target.value;
+    let value = event.target.value;
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
     setFormData ({
       ...formData,
       [name]:value,
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
       type="text"
       name="firstName"
@@ -28,6 +37,13 @@ function Form() {
       value={formData.lastName}
       onChange={handleChange}
       />
+      <input
+      type="checkbox"
+      name="admin"
+      onChange={handleChange}
+      checked={formData.admin}
+      />
+      <button type="submit">Submit</button>
     </form>
   );
 }
